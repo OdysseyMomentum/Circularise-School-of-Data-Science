56eadeb07b71ead7be362428c76757ddaf4b9162
IRiViI <rckvnk@gmail.com>
2020-11-14 19:42:59 +0100

    update with home

--- mobile-frontend/mobile-frontend/src/app/pages/home/home.component.ts
+++ mobile-frontend/mobile-frontend/src/app/pages/home/home.component.ts
@@ -7,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+	public people = [
+	{
+		name: "Alice",
+		image: "/assets/images/placeholder_people/person2.jpg",
+	},
+	{
+		name: "Bob",
+		image: "/assets/images/placeholder_people/person11.jpg",
+	},
+	{
+		name: "Charlie",
+		image: "/assets/images/placeholder_people/person12.jpg",
+	},
+	{
+		name: "Dave",
+		image: "/assets/images/placeholder_people/person9.jpg",
+	},
+	{
+		name: "Eve",
+		image: "/assets/images/placeholder_people/person5.jpg",
+	},
+	{
+		name: "Frank",
+		image: "/assets/images/placeholder_people/person6.jpg",
+	}]
+
   constructor() { }
 
   ngOnInit(): void {
