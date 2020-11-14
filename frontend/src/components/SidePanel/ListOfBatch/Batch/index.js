d6635222a21d276c56d1d6e7ed22112252b80d35
Adrien Rahier <adrien.rahier@gmail.com>
2020-11-14 22:25:07 +0100

    Quick improvements to the styles

--- frontend/src/components/SidePanel/ListOfBatch/Batch/index.js
+++ frontend/src/components/SidePanel/ListOfBatch/Batch/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./styles.css";
+import CTA from "../../../shared/CTA";
 
 export default class Batch extends React.Component {
   state = {
     isSelected: false,
   };
 
-  selectBatch = () => {
+  selectPreview = () => {
     this.props.callFlyToMarker(this.props.feature);
+  };
+
+  selectAdd = () => {
     this.props.updateTotalKg(
       this.state.isSelected
         ? -this.props.feature.properties.weight
@@ -18,12 +22,18 @@ export default class Batch extends React.Component {
 
   render() {
     return (
-      <li
-        className={`item ${this.state.isSelected ? "item--selected" : ""}`}
-        onClick={() => this.selectBatch()}
-      >
+      <li className={`item ${this.state.isSelected ? "item--selected" : ""}`}>
         <p className="title">{this.props.feature.properties.name}</p>
         <p>Weight: {this.props.feature.properties.weight}</p>
+        <CTA
+          buttonColor="blue"
+          text={"See on map"}
+          onClick={() => this.selectPreview()}
+        />
+        <CTA
+          text={`${this.state.isSelected ? "Remove from" : "Add to"} cart`}
+          onClick={() => this.selectAdd()}
+        />
       </li>
     );
   }
