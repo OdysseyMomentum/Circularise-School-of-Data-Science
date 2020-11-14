85d863476badf5fc24ffc20d0383afaded1d7fcd
IRiViI <rckvnk@gmail.com>
2020-11-15 00:14:48 +0100

    Create event added

---
+++ mobile-frontend/mobile-frontend/src/app/pages/active-event/active-event.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ActiveEventComponent } from './active-event.component';
+
+describe('ActiveEventComponent', () => {
+  let component: ActiveEventComponent;
+  let fixture: ComponentFixture<ActiveEventComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ActiveEventComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ActiveEventComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
