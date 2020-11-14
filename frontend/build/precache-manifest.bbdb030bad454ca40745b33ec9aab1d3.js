dcc0fff63eb3e7521f0ff4b78f7efd14ad3cf528
Adrien Rahier <adrien.rahier@gmail.com>
2020-11-14 22:32:56 +0100

    Making a stable build

---
+++ frontend/build/precache-manifest.bbdb030bad454ca40745b33ec9aab1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8564880c3683a43039ab23a9d54fcfc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2ead6bbb16b01c5eae",
+    "url": "/static/css/main.b34b0d2b.chunk.css"
+  },
+  {
+    "revision": "8ce658c90bca0c83a3dc",
+    "url": "/static/js/2.cdbd9ed3.chunk.js"
+  },
+  {
+    "revision": "fd2ead6bbb16b01c5eae",
+    "url": "/static/js/main.7f3f6905.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab37ccfcc3b283702ea2e06dba9d7f8a",
+    "url": "/static/media/mockData.ab37ccfc.geojson"
+  }
+]);
