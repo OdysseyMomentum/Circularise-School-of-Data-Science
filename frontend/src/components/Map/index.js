2bc639bc516407691681881a73a353629c8d37a0
Adrien Rahier <adrien.rahier@gmail.com>
2020-11-14 14:04:41 +0100

    Adding a bit of interaction to the map

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -16,14 +16,27 @@ export default class Map extends React.Component {
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom,
     });
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
   }
   render() {
     mapboxgl.accessToken =
       "pk.eyJ1IjoiYWVnaXoiLCJhIjoiY2tlZTNwcDJ3MHBlOTJybHRzaWduNGRjaCJ9.E4R2Mr5QMvQnIWt8StxRQg";
     return (
-      <header className="map">
+      <div className="map">
         <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
-      </header>
+        <div className="sidebarStyle">
+          <div>
+            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
+            {this.state.zoom}
+          </div>
+        </div>
+      </div>
     );
   }
 }
