acbf73747bb19e8567c97b843ddbeeb5ef805e60
Adrien Rahier <adrien.rahier@gmail.com>
2020-11-15 11:31:44 +0100

    Commiting the build

---
+++ frontend/build/precache-manifest.3ae26bd58b34d172e59bc51b7d6f3e9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b92516e34f9431c63aa32c42e139d7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1aa07bc6d2cf54b6a75",
+    "url": "/static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "38a5eb639d042969686b",
+    "url": "/static/css/main.ed975012.chunk.css"
+  },
+  {
+    "revision": "c1aa07bc6d2cf54b6a75",
+    "url": "/static/js/2.20bf51a3.chunk.js"
+  },
+  {
+    "revision": "38a5eb639d042969686b",
+    "url": "/static/js/main.92aed127.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af99593539636fcfc0c4b4eaea350341",
+    "url": "/static/media/mockData.af995935.geojson"
+  }
+]);
