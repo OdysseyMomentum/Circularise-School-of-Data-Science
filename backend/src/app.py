1bb07a87e4b5d8cf7875dc4f8ad9317511ac2078
sharvandt <sharvan.dt@gmail.com>
2020-11-15 13:15:47 +0100

    added CORS functionality

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
 from handlers.blockchain import blockchain
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.mount("/tokens", blockchain)
